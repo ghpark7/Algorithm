9dc796445471f6a29ea669eceb46085e30a9c72e
Geonhwi Park <ghpark@g.hongik.ac.kr>
2024-06-26 14:33:44 +0900

    [Silver IV] Title: 상근이의 여행, Time: 1168 ms, Memory: 240596 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+            for (int j = 0; j < M; j++) {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+            }
+            System.out.println(N - 1);
+        }
+    }
+}
