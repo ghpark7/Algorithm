b69d9f1f19861d4cf7919bfde722c57897ba4015
Geonhwi Park <ghpark@g.hongik.ac.kr>
2025-04-29 14:08:34 +0900

    [Gold V] Title: 행복 유치원, Time: 572 ms, Memory: 50588 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -1,39 +1,39 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		int[] height = new int[n];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			height[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		if (k >= n) {
-            System.out.println(0);
-            return;
-        }
-
-        int[] diffs = new int[n-1];
-        for (int i = 0; i < n-1; i++) {
-            diffs[i] = height[i+1] - height[i];
-        }
-
-        Arrays.sort(diffs);
-        
-        int totalCost = 0;
-        for (int i = 0; i < n-k; i++) {
-            totalCost += diffs[i];
-        }
-
-        System.out.println(totalCost);
-	}
-
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int N = Integer.parseInt(st.nextToken());
+    	int K = Integer.parseInt(st.nextToken());
+    	int[] babies = new int[N];
+    	int[] diffs = new int[N-1];
+    	st = new StringTokenizer(br.readLine());
+    	for (int i=0;i<N;i++) {
+    		babies[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	int end = babies[N-1] - babies[0];
+    	if (K == 1) {
+    		System.out.println(end);
+    		return;
+    	}
+    	
+    	int sum = 0;
+    	
+    	for (int i=1;i<N;i++) {
+    		diffs[i-1] = babies[i] - babies[i-1];
+    		sum += diffs[i-1];
+    	}
+    	
+    	Arrays.sort(diffs);
+    	
+    	for (int i=diffs.length - 1;i >= diffs.length - (K-1) ;i--) {
+    		sum -= diffs[i];
+    	}
+    	
+    	System.out.println(sum);
+    }
 }
